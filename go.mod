f8b9eb79e0601973138b93801a1d7a0f3b7546c3
emre <emre_erinc@hotmail.com>
2020-09-01 01:51:19 +0300

    added file download section

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module gorive
 go 1.14
 
 require (
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1
+	github.com/basgys/goxml2json v1.1.0
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.30.0
--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -115,6 +117,7 @@ func fetch(service *drive.Service, count int64, nextPageToken string) {
 		fmt.Println("No files found.")
 	} else {
 		tree = make(map[string][]string)
+		// append parameters to file tree
 		for _, i := range result.Files {
 			if strings.Contains(i.MimeType, "folder") {
 				tree[i.Id] = append(tree[i.Name], "| "+i.Name)
@@ -124,21 +127,22 @@ func fetch(service *drive.Service, count int64, nextPageToken string) {
 		}
 	}
 
+	// write file tree
 	for _, value := range tree {
 		for _, v := range value {
 			fmt.Printf("%s\n", v)
 		}
 	}
 
+	// continuous
 	if result.NextPageToken != "" {
-		fmt.Printf("\nContinue to fetch %d item, Press Enter...\n", count)
-
+		fmt.Printf("\nContinue to fetch %d item, \nPress Enter...\n", count)
+		scanner := bufio.NewScanner(os.Stdin)
 		for {
 			// only read single characters, the rest will be ignored!!
 			consoleReader := bufio.NewReaderSize(os.Stdin, 1)
-			fmt.Print(">")
+			fmt.Print("\n>")
 			input, _ := consoleReader.ReadByte()
-
 			ascii := input
 
 			// ESC = 27 and Ctrl-C = 3
@@ -147,9 +151,114 @@ func fetch(service *drive.Service, count int64, nextPageToken string) {
 				os.Exit(0)
 			}
 
+			if ascii == 100 {
+				fmt.Print("Download >> ")
+				scanner.Scan()
+
+				fileName := scanner.Text()
+				for _, i := range result.Files {
+					//fmt.Printf("file name : %s ---- desired file name : %s\n",i.Name, file)
+					if i.Name == fileName {
+						file, err := service.Files.Get(i.Id).Fields("*").Do()
+
+						b, err := ioutil.ReadFile("credentials.json")
+						if err != nil {
+							log.Fatalf("Unable to read client secret file: %v", err)
+						}
+
+						config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+						if err != nil {
+							log.Fatalf("Unable to parse client secret file to config: %v", err)
+						}
+						client := getClient(config)
+
+						dlFile, err := DownloadFile(client.Transport, file)
+						if err != nil {
+							fmt.Printf("an error occurred while '%s' dowloading\n", i.Name)
+						}
+
+						fmt.Printf("Download : %s", dlFile)
+					}
+				}
+			}
+
 			if ascii == 10 {
 				fetch(service, count, result.NextPageToken)
 			}
 		}
 	}
+}
+
+func DownloadFile(t http.RoundTripper, f *drive.File) (string, error) {
+	// t parameter should use an oauth.Transport
+	downloadUrl := f.WebContentLink
+	if downloadUrl == "" {
+		// If there is no downloadUrl, there is no body
+		fmt.Printf("An error occurred: File is not downloadable")
+		return "", nil
+	}
+	req, err := http.NewRequest("GET", downloadUrl, nil)
+	if err != nil {
+		fmt.Printf("An error occurred: %v\n", err)
+		return "", err
+	}
+	resp, err := t.RoundTrip(req)
+	// Make sure we close the Body later
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Printf("An error occurred: %v\n", err)
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("An error occurred: %v\n", err)
+		return "", err
+	}
+
+	//res := strings.TrimLeft(string(body), "<HEAD>\n<TITLE>Moved Temporarily</TITLE>\n</HEAD>\n<BODY BGCOLOR=\"#FFFFFF\" TEXT=\"#000000\">\n<H1>Moved Temporarily</H1>\nThe document has moved <A HREF=\"")
+	//fmt.Println(res)
+
+	// Moved Temporarily
+	//xml := strings.NewReader(string(body))
+	//result, err := xj.Convert(xml)
+	//if err != nil {
+	//	panic("An error occurred while xml parsing")
+	//}
+
+
+
+	//result1 :=strings.ReplaceAll(result.String(), "{\"HTML\": {\"HEAD\": {\"TITLE\": \"Moved Temporarily\"}, \"BODY\": {\"#content\": \".\", \"A\": {\"#content\": \"here\", \"-HREF\": \"", "")
+	//result1 = strings.ReplaceAll(result1, "\"}, \"-BGCOLOR\": \"#FFFFFF\", \"-TEXT\": \"#000000\", \"H1\": \"Moved Temporarily\"}}}", "")
+	//
+
+	//req, err = http.NewRequest("GET", result1, nil)
+	//if err != nil {
+	//	fmt.Printf("An error occurred: %v\n", err)
+	//	return "", err
+	//}
+	//resp, err = t.RoundTrip(req)
+	//// Make sure we close the Body later
+	//defer resp.Body.Close()
+	//if err != nil {
+	//	fmt.Printf("An error occurred: %v\n", err)
+	//	return "", err
+	//}
+	//body, err = ioutil.ReadAll(resp.Body)
+	//if err != nil {
+	//	fmt.Printf("An error occurred: %v\n", err)
+	//	return "", err
+	//}
+
+	path := RootDirectory() + "/" + f.Name
+	err = ioutil.WriteFile(path, body, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body), nil
+}
+
+func RootDirectory() string {
+	_, file, _, _ := runtime.Caller(0)
+	return path.Join(path.Dir(file))
 }
